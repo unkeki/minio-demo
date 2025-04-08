9a441207e8ecc5115fe996a71ea917f87ae42683
fangzeqiang <279862291@qq.com>
2025-04-08 16:56:07 +0800

    1

--- src/main/java/com/example/miniodemo/config/MinioConfig.java
+++ src/main/java/com/example/miniodemo/config/MinioConfig.java
@@ -2,6 +2,7 @@ package com.example.miniodemo.config;
 
 import io.minio.MinioClient;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,7 @@ public class MinioConfig {
     private String bucketName;
 
     @Bean
+    @ConditionalOnMissingBean
     public MinioClient minioClient(){
         return MinioClient.builder()
                 .endpoint(url)
